a04de1278a791f9cdfa317eb2d2b8d859d729930
nilpeep <niluferk038@gmail.com>
2024-01-11 22:07:55 +0300

    final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>
-        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, sed.</h1>
-    </div>
+    <div>kjashfkjahsfkjmznxvoiasjdklajsd</div>
   )
 }
