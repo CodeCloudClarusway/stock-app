b3ed22bb04fc8c92ca59615ff789a9bc1a2c8a7c
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-01-12 17:29:30 +0300

    logout

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -1,31 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user:'',
-    token:'',
-    error:false,
-    loading:false
-}
+  user: "",
+  token: "",
+  error: false,
+  loading: false,
+};
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
-    fetchStart:(state)=>{
-        state.loading=true
+    fetchStart: (state) => {
+      state.loading = true;
     },
-    fetchFail:(state)=>{
-        state.loading=false
-        state.error=true
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
     },
-    loginSuccess:(state,{payload})=>{
-        state.loading=false
-        state.token=payload.token
-        state.user=payload.user.username
-    }
-  }
+    loginSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.token = payload.token;
+      state.user = payload.user.username;
+    },
+    logoutSuccess: (state) => {
+      state.loading = false;
+      state.token = "";
+      state.user = "";
+    },
+  },
 });
 
-export const {fetchFail,fetchStart,loginSuccess} = authSlice.actions
+export const { fetchFail, fetchStart, loginSuccess, logoutSuccess } =
+  authSlice.actions;
 
-export default authSlice.reducer
+export default authSlice.reducer;
--- src/services/useAuthCalls.jsx
+++ src/services/useAuthCalls.jsx
@@ -1,13 +1,19 @@
 import axios from "axios";
 import React from "react";
-import { useDispatch } from "react-redux";
-import { fetchStart, loginSuccess } from "../features/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchFail,
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+} from "../features/authSlice";
 import { useNavigate } from "react-router-dom/dist";
 
 export const useAuthCalls = () => {
   const navigate = useNavigate();
   const BASE_URL = process.env.REACT_APP_BASE_URL;
   const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
   const login = async (userInfo) => {
     dispatch(fetchStart());
     try {
@@ -20,5 +26,17 @@ export const useAuthCalls = () => {
       console.log(error);
     }
   };
-  return { login };
+  const logout = async () => {
+    dispatch(fetchStart());
+
+    try {
+      await axios(`${BASE_URL}/auth/logout`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      dispatch(logoutSuccess());
+    } catch (error) {
+      dispatch(fetchFail());
+    }
+  };
+  return { login, logout };
 };
