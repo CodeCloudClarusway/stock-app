80e41ca54f3824f082ae3d1769379e1b8e178c64
Vildan Beyza Çetin <vcetin1356@gmail.com>
2024-01-14 18:57:57 +0300

    page routes added

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom/dist";
 import PrivateRouter from "./PrivateRouter";
 import { Home } from "../pages/Home";
+import Brands from "../pages/Brands";
+import Products from "../pages/Products";
+import Purchases from "../pages/Purchases";
+import Firms from "../pages/Firms";
+import Sales from "../pages/Sales";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 
@@ -11,7 +16,14 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            <Route index path="" element={<Home/>}/>
+            <Route  path="products" element={<Products/>}/>
+            <Route  path="brands" element={<Brands/>}/>
+            <Route  path="purchases" element={<Purchases/>}/>
+            <Route  path="sales" element={<Sales/>}/>
+            <Route  path="firms" element={<Firms/>}/>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
