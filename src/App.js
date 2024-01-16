5f6ed7111f8dba47c30cbf4e10bef290ad773b6e
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-01-16 21:25:15 +0300

    persist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Provider } from "react-redux";
 import Login from "./pages/Login";
-import { store } from "./App/store";
+import { persistor, store } from "./App/store";
 import AppRouter from "./Router/AppRouter";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={store}>
-      <AppRouter />
+      <PersistGate loading={null} persistor={persistor}>
+        <AppRouter />
+      </PersistGate>
     </Provider>
   );
 }
--- src/App/store.js
+++ src/App/store.js
@@ -1,7 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import authSlice from "../features/authSlice";
 import stockReducer from "../features/stockSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+ const persistedReducer = persistReducer(persistConfig, authSlice);
+
 export const store = configureStore({
-  reducer: { auth: authSlice, stock: stockReducer },
+  reducer: { auth: persistedReducer, stock: stockReducer },
 });
+
+export const persistor = persistStore(store);
+
+
